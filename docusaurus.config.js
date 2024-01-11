8fa888b2609e894c155c9e0ec3ac792e85e88de8
Nico Diz <nicodiz.literaturayeducacion@proton.me>
2024-01-11 17:06:02 -0300

    link a github

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -80,7 +80,7 @@ const config = {
           },
           {to: '/blog', label: 'Blog', position: 'left'},
           {
-            href: 'https://github.com/facebook/docusaurus',
+            href: 'https://github.com/nicodiz/literatura-y-educacion',
             label: 'GitHub',
             position: 'right',
           },
