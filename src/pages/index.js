2666c65c973da9f2adb9c7ff14c5b1e18afe734a
Nico Diz <nicodiz.literaturayeducacion@proton.me>
2024-01-19 17:04:15 -0300

    inicio

--- src/pages/index.js
+++ src/pages/index.js
@@ -29,11 +29,10 @@ function HomepageHeader() {
 }
 
 export default function Home() {
-  const {siteConfig} = useDocusaurusContext();
   return (
     <Layout
-      title={`Hello from ${siteConfig.title}`}
-      description="Description will go into a meta tag in <head />">
+      title="Inicio"
+      description="Material de literatura y educación">
       <HomepageHeader />
       <main>
         <HomepageFeatures />
